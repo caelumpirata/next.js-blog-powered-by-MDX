ee81f0da80d314337c7d3371dddd7807ffadc0e5
Caelum Pirata <caelumpirata@gmail.com>
2023-12-01 23:48:49 +0530

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 
 export default function Document() {
@@ -7,8 +6,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <link rel="icon" type="image/webp" sizes="128x128" href="/favicon.png" />
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7278426684287328"
-           crossOrigin="anonymous"></Script>
       </Head>
       <body className='antialiased'>
         <Main />
