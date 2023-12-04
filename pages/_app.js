9d7823cb721e5313ca90b1a5011844190c036ad8
Caelum Pirata <caelumpirata@gmail.com>
2023-12-04 14:03:49 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,7 @@ import Layout from "@/components/Layout";
 export default function MyApp({ Component, pageProps }) {
   return (
       <ThemeProvider attribute="class">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Layout {<Component {...pageProps} />} />
       </ThemeProvider>
   );
 }
