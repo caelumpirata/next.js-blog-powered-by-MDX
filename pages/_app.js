198b0c291ae96b74bcf9175c6a2438d6dfddf703
Caelum Pirata <caelumpirata@gmail.com>
2023-12-06 17:39:33 +0530

    displace _document.js in root

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import Layout from "@/components/layout";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
       <ThemeProvider attribute="class">
         <Layout >
+        <Head>
+        <link rel="icon" type="image/webp" sizes="128x128" href="/trees.svg" />
+      </Head>
           <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
