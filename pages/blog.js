ffe578affb8b87af2fde4faafd6e9c4ee0391639
Caelum Pirata <caelumpirata@gmail.com>
2023-12-08 14:54:55 +0530

    implement view counter for /blog

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Head from "next/head";
-
+import useSWR from "swr";
 import Date from "@/components/date";
 import fs from "fs";
 import matter from "gray-matter";
@@ -39,42 +39,47 @@ export default function Home({ posts }) {
         />
       </Head>
 
-
       <article className="flex-grow">
         <div className="mx-auto max-w-2xl px-4 md:px-0 pt-16 sm:pt-24">
           <h1 className="text-black dark:text-white text-3xl font-extrabold tracking-tight sm:text-4xl">
             Blog
           </h1>
           <div>
-            {posts.map((post) => (
-              <Link
-                className="block mt-8 p-4 bg-gray-100 dark:bg-gray-900 rounded-md sm:p-8"
-                key={post.filePath}
-                as={`/blog/${post.filePath.replace(/\.mdx?$/, "")}`}
-                href={`/blog/[slug]`}
-              >
-                <div className="flex-col">
-                  <time className="pb-2 dark:text-gray-400 text-gray-500 whitespace-nowrap text-sm">
-                    <Date dateString={post.data.date} />
-                  </time>
-                  <h3 className="dark:text-white text-xl font-bold">
-                    {post.data.title}{" "}
-                  </h3>
-                </div>
-                <p className="mt-3 dark:text-gray-300 text-gray-600">
-                  {post.data.description}
-                </p>
-              </Link>
-            ))}
-            {/* {posts.map((post) => (
-              <Link className="block mt-8 p-4 bg-gray-100 dark:bg-gray-900 rounded-md sm:p-8" key={post.uuid} href={`/blog/${post.slug}`}>
-                <div className="flex-col">
-                    <time className="pb-2 dark:text-gray-400 text-gray-500 whitespace-nowrap text-sm"><Date dateString={post.published_at} /></time>
-                    <h3 className="dark:text-white text-xl font-bold">{post.title} </h3>   
-                </div>
-                <p className="mt-3 dark:text-gray-300 text-gray-600">{post.excerpt}</p>
-              </Link>
-          ))} */}
+            {posts.map((post) => {
+              const slug = post.filePath.replace(/\.mdx?$/, "");
+
+              const fetcher = (url) => fetch(url).then((res) => res.text());
+              const { data: viewCount, error } = useSWR(
+                `/api/redisGet?id=/blog/${slug}`,
+                fetcher
+              );
+
+              if (error) {
+                console.error("Error fetching data:", error);
+              }
+
+              return (
+                <Link
+                  className="block mt-8 p-4 bg-gray-100 dark:bg-gray-900 rounded-md sm:p-8"
+                  key={post.filePath}
+                  as={`/blog/${slug}`}
+                  href="/blog/[slug]"
+                >
+                  <div className="flex-col">
+                    <time className="pb-2 dark:text-gray-400 text-gray-500 whitespace-nowrap text-sm">
+                      <Date dateString={post.data.date} />
+                    </time>
+                    <h3 className="dark:text-white text-xl font-bold">
+                      {post.data.title}
+                    </h3>
+                  </div>
+                  <p className="mt-3 dark:text-gray-300 text-gray-600">
+                    {post.data.description}
+                  </p>
+                  {viewCount && <p>{viewCount}</p>}
+                </Link>
+              );
+            })}
           </div>
         </div>
       </article>
